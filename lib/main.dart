8721c203253cae225943a0199170c1a986b3f0a2
aida <yuaida.12@gmail.com>
2022-06-07 19:44:54 +0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kuis/data/question_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -24,8 +26,111 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Color mainColor = Color(0xFF252c4a);
+  Color secondColor = Color(0xFF117eeb);
+  PageController _controller = PageController(initialPage: 0);
+  bool isPressed = false;
+  Color isTrue = Colors.green;
+  Color isWrong = Colors.red;
+  Color btnColor = Color(0xFF117eeb);
+  int score = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: PageView.builder(
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Question ${index + 1}/${questions.length}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 28.0,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.white,
+                  height: 8.0,
+                  thickness: 1.0,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  questions[index].question!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                for (int i = 0; i < questions[index].answer!.length; i++)
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 18.0),
+                    child: MaterialButton(
+                      shape: StadiumBorder(),
+                      color: isPressed
+                          ? questions[index].answer!.entries.toList()[i].value
+                              ? isTrue
+                              : isWrong
+                          : secondColor,
+                      padding: EdgeInsets.symmetric(vertical: 18.0),
+                      onPressed: isPressed
+                          ? () {}
+                          : () {
+                              setState(() {
+                                isPressed = true;
+                              });
+                              if (questions[index]
+                                  .answer!
+                                  .entries
+                                  .toList()[i]
+                                  .value) {
+                                score += 10;
+                              }
+                            },
+                      child: Text(
+                        questions[index].answer!.keys.toList()[i],
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(),
+                      child: Text(
+                        "Next Question",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
