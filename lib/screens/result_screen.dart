df3b1a06d7f71315ac5f8af58a4b224af1763453
aida <yuaida.12@gmail.com>
2022-06-09 17:26:50 +0700

    score

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatefulWidget {
+  final int score;
+  const ResultScreen(this.score, {Key? key}) : super(key: key);
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Color mainColor = Color(0xFF252c4a);
+    Color secondColor = Color(0xFF117eeb);
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Congratulation",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "your score is",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Text(
+              "${widget.score}",
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 80.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
