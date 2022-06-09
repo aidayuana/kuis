20cd86a9094f5e977e6c4ae9878ad6680c515958
aida <yuaida.12@gmail.com>
2022-06-09 17:48:55 +0700

    repeat_quizz

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kuis/main.dart';
 
 class ResultScreen extends StatefulWidget {
   final int score;
@@ -49,6 +50,19 @@ class _ResultScreenState extends State<ResultScreen> {
                 fontSize: 80.0,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              elevation: 0.0,
+              color: Colors.orange,
+              textColor: Colors.white,
+              child: Text("Repeat the Quizz"),
             ),
           ],
         ),
